d264cf116eb35e8a590dedb7f484328b003cc365
mike <mike@familien-jahn.dk>
2020-11-21 19:03:46 +0100

    raspberry only

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import glob
 import json
 import sys
 import time
+import traceback
 from datetime import datetime
 import logging
 
@@ -9,15 +10,8 @@ import firebase_admin
 import serial
 from firebase_admin import credentials, firestore
 
-from firebaseTest import myFirebase
+import Adafruit_DHT
 
-
-# firestore_db.collection(u'songs').add({'song': 'Imagine', 'artist': 'John Lennon'})
-# firestore_db.collection(path).add({'time': datetime.now(), 'temperature': 22, 'humidity': 40})
-
-# snapshots = list(firestore_db.collection(path).get())
-# for snapshot in snapshots:
-#     print(snapshot.to_dict())
 class MyFirebase:
     def __init__(self):
         self.path = "HomeTemperatures"
@@ -27,8 +21,9 @@ class MyFirebase:
         self.firestore_db = firestore.client()
 
     def addReading(self, Temperature, Humidity):
-        self.firestore_db.collection(self.path).add(
-            {'time': datetime.now(), 'temperature': Temperature, 'humidity': Humidity})
+        dic = {'time': datetime.now(), 'temperature': Temperature, 'humidity': Humidity}
+        self.firestore_db.collection(self.path).add(dic)
+        print(dic)
 
     def readAllAndPrint(self):
         snapshots = list(self.firestore_db.collection(self.path).get())
@@ -36,53 +31,17 @@ class MyFirebase:
             print(snapshot.to_dict())
 
 
-class MySerial:
-    def __init__(self):
-        port = self.getPorts()
-        if (port):
-            self.com = serial.Serial(port=port[0], baudrate=9600, timeout=1)
-            self.rawData = ""
-        else:
-            raise serial.SerialException("No port found")
-
-    def readLine(self) -> dict:
-        self.rawData = self.com.readline().decode("utf-8")
-        return json.loads(self.rawData)
-
-    def getPorts(self):
-        if sys.platform.startswith('win'):
-            ports = ['COM%s' % (i + 1) for i in range(256)]
-        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-            # this excludes your current terminal "/dev/tty"
-            ports = glob.glob('/dev/tty[A-Za-z]*')
-        elif sys.platform.startswith('darwin'):
-            ports = glob.glob('/dev/tty.*')
-        else:
-            raise EnvironmentError('Unsupported platform')
-
-        result = []
-        # checks all ports and append available ports to result
-        for port in ports:
-            try:
-                s = serial.Serial(port)
-                s.close()
-                result.append(port)
-            except (OSError, serial.SerialException):
-                pass
-        return result
-
-
 if __name__ == '__main__':
     logger = logging.getLogger('log')
     logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     db = MyFirebase()
-    ser = MySerial()
     while(True):
         try:
-            db.addReading(**ser.readLine())
+            humidity, temperature = Adafruit_DHT.read_retry(11, 4,delay_seconds=0)
+            db.addReading(temperature, humidity)
             time.sleep(1*60*30)
         except Exception as e:
             logging.error("Loop failed", exc_info=True)
-            logging.error(f"Raw data{ser.rawData}", exc_info=True)
+            traceback.print_stack()
             time.sleep(1 * 60)
-    # db.readAllAndPrint()
+
